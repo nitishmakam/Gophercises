167b25d23399fe91711787c9133eb490cd69e6cd
Nitish Makam <nitishmakam@ITSupportsMBP5.bbrouter>
2022-11-29 19:07:23 +0530

    Gophercises: Quiz: Adding problems and minor refactoring

--- quiz/main.go
+++ quiz/main.go
@@ -16,19 +16,21 @@ type problem struct {
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "A CSV file containing problems and solutions in the format'question,answer'")
 	flag.Parse()
+
 	csvFile, err := os.Open(*csvFileName)
 	if err != nil {
 		raiseError(fmt.Sprintf("An error occurred while trying to open file: %s\n", *csvFileName))
 	}
 
-	lines := csv.NewReader(csvFile)
-	records, err := lines.ReadAll()
+	r := csv.NewReader(csvFile)
+	lines, err := r.ReadAll()
 	if err != nil {
 		raiseError("An error occurred while trying to read the CSV.")
 	}
 
-	problems := parseLines(records)
-	var correct int
+	problems := parseLines(lines)
+	correct := 0
+
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d: %s\n", i+1, problem.question)
 		var answer string
@@ -42,9 +44,9 @@ func main() {
 
 }
 
-func parseLines(records [][]string) []problem {
-	problems := make([]problem, len(records))
-	for i, record := range records {
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, record := range lines {
 		problems[i] = problem{
 			question: record[0],
 			answer:   strings.TrimSpace(record[1]),
